96c666518e57e41885c9dd81b87d948fd75d7075
= <ledung2020604439@gmail.com>
2023-02-15 19:22:30 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import { useState } from 'react';
 
 
 function App() {
+  const [work, setWork]=useState('')
+
   return (
-    <div className="App">
+    <div className="flex gap-8 h-screen items-center border border-red-500 justify-center">
+
       
-      app
+      <input
+      type="text"
+      className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+      value={work}
+      onChange={e => setWork(e.target.value)}
+
+      />
+      <button
+      type="button"
+      className="outline-none px-4 py-2 bg-blue-500 rounded-md text-while"
+      >
+      Add
+      </button>
     </div>
   );
 }
